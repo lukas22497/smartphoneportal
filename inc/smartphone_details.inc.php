34252daa77f0dba475a62ed5fe2e241d3cb93f10
lukas22497 <lukas_mueller@outlook.de>
2015-05-02 21:41:31 +0200

    Abfrage mit einem weiteren spec klappt
    Abfrage klappt. Leider liefert sie bei mir aus irgendeinem Grund alte
    Ergebnisse und gibt auch nur die erste Zeile aus, muss getestet werden.
    Ist aber irgendwie komisch.

--- inc/smartphone_details.inc.php
+++ inc/smartphone_details.inc.php
@@ -5,7 +5,7 @@
 	
 	//SQl-Statement
 	$sql = 'SELECT 
-			s.id, s.name, c.category, o.os
+			s.id, s.name, c.category, o.os, w.wlan
             FROM
             smartphone as s
             JOIN
@@ -14,13 +14,19 @@
             JOIN
             os as o
             on s.ID = o.ID
+            JOIN
+            specs as sp
+            on s.ID = sp.ID
+            JOIN
+            wlan as w
+            on sp.ID = w.ID            
             WHERE s.name LIKE \'%'.$_GET['smartphone'].'%\'';
             // Hier die richtige Variable verwenden. Vielleicht die Smartphone-ID benutzen oder den ausgewählten Namen.
             // in die Abfrage müssen noch die anderen Spezifikationen eingebaut werden.
 	$erg = $db->query($sql) or die ("Fehlermeldung=".$db->error()); // $erg ist ein Mysql-Link, der auf ein Ergebnis in der MySQL-Datenbank zeigt.
     echo "<table>";
 	echo "<thead>";
-	echo "<tr><th>Name</th><th>Kategorie</th><th>Betriebssystem</th></tr>";
+	echo "<tr><th>Name</th><th>Kategorie</th><th>Betriebssystem</th><th>WLAN</th></tr>";
 	echo "</thead>";
 	echo "<tbody>";
 	while (($row = $erg->fetch_assoc()) !== NULL) {
@@ -28,7 +34,8 @@
 		$a=$row["name"];
 		$b=$row["category"];
         $c=$row["os"];
-		echo "<tr><td>$a</td><td>$b</td><td>$c</td></tr>";
+        $d=$row["wlan"];
+		echo "<tr><td>$a</td><td>$b</td><td>$c</td><td>$d</td></tr>";
 	}
 	echo "</tbody>";
 	echo "</table>";
