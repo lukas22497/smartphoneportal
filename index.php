8c136d9e6c50568275616321bb7bde91a73ff538
root <root@R84FN79.intranet.de>
2015-05-03 22:07:17 +0200

    Angleichen

--- index.php
+++ index.php
@@ -1,111 +1,107 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<link type="text/css" href="css/style.css" rel="stylesheet" media="screen" />
-<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<!-- 	html5 + CSS 3 Template created by miss monorom  http://intensivstation.ch 2011 -->
 
-<title>Smartphoneportal</title>
-</head>
+ <head>
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link type="text/css" href="css/style.css" rel="stylesheet" media="screen" />
+ 
+  <title>Smartphone-Portal</title>
+ </head>
+
 <body>
-<div id="container">
-	<header>
-		<h2>
-			Smartphoneportal
-		</h2>
-	</header>
-	<nav>
-		<h2>
-			nav: links
-		</h2>
-		<ul>
-			<li><a href="index.php?navi=1" title="Normale Darstellung">Kneipen anzeigen</a></li>
-			<li><a href="index.php?navi=2" title="jQuery-Darstellung">Kneipen anzeigen</a></li>
-			<li><a href="index.php?navi=3" title="animierte jQuery-Darstellung">Kneipen anzeigen</a></li>
-			<li><a href="index.php?navi=4">Kneipen hinzufügen</a></li>
-		</ul>
-	</nav>
-	<aside>
-		<h2>
-			aside: Suche
-		</h2>
-		<?php
-			echo "<form method='post' action='{$_SERVER['PHP_SELF']}'>";
-		?>
-			<input type=text name=suchstring size=25>
-			<br/>
-			<input type="submit" value="Suchen">
-			<input type="reset">
-			<input type="hidden" name="sent" value="1">
-			</form>
-			<?php
-			echo "<form method='post' action='index.php'>
-				<table>
-				<tr>
-					<td>
-						<input id='tags' type=text name=suchstring size=25>
-					</td>
-				</tr>
-					<td>
-						<input type='submit' value='Absenden'>
-						<input type='hidden' name='sent' value='1'>
-					</td>
-				</tr>
-				</table>";
-			?>
-	</aside>
-	<section id="content">
-		<article>
-			<h2>
-				Article: Inhalt
-			</h2>
-			<?php
-			$navigation = isset($_GET['navi']) ? $_GET['navi'] : '0'; // Kurzform if-then-else-Verweisung
-			//echo $navigation;
-			switch ($navigation) {
-				case "0":
-					echo "<h2>Willkommen im Smartphoneportal!</h2>";
-					break;
-				case "1":
-					echo "<h2>Smartphones anzeigen (normal)</h2>";
-					include ("inc/smartphoneanzeigen1.inc.php");
-					break;
-				case "2":
-					echo "<h2>Smartphones anzeigen (jQuery-Tablesorter)</h2>";
-					include ("inc/smartphoneanzeigen2.inc.php");
-					break;
-				case "3":
-					echo "<h2>Smartphones anzeigen (animiertes Tablesort)</h2>";
-					include ("inc/smartphoneanzeigen3.inc.php");
-					break;
-				case "4":
-					echo "<h2>Smartphones hinzufügen</h2>";
-					include ("inc/smartphoneanzeigen.inc.php");
-					break;
-                //Detailansicht
-                case "10":
-                    echo "<h2>Detailansicht</h2>";
-                    include ("inc/smartphone_details.inc.php");
-                    break;
-				} // Ende Switch-Case-Anweisung	
-				
-				// Überprüfung auf Ausführung des Suchformulars
-				$sent = isset($_POST['sent']) ? $_POST['sent'] : '';
-				$suche = isset($_POST['suchstring']) ? $_POST['suchstring'] : '';
-				if ($sent and $suche) {
-					echo "<h2>Smartphone suchen</h2>";
-					include("inc/suchen.inc.php");
-				}
-			?>			
-		</article>
-	</section>
- 	<footer>		
-		<h2>
-			Footer
-		</h2>
-	<p>Template changed by Lukas</a></p>
-	</footer>
-</div>
+
+ <div id="container">
+
+  <header>
+    <h2>Willkommen im Smartphone-Portal</h2>
+    <img src="img/header.png"/>
+  </header>
+
+  <nav>
+    <h1>Men&uuml;</h1>
+    <ul>
+     <li><a href="index.php?navi=1">Smartphones anzeigen</a></li>
+     <li><a href="index.php?navi=2">Smartphones anzeigen</a></li>
+     <li><a href="index.php?navi=3">Smartphones anzeigen</a></li>
+     <li><a href="index.php?navi=4">Smartphones hinzuf&uuml;gen</a></li>
+    </ul>
+  </nav>
+
+  <aside>
+    <h2>Suche</h2>
+    <?php
+      echo "<form method='post' action='{$_SERVER['PHP_SELF']}'>";
+    ?>
+      <input type="text" name="suchstring" size="24"><br />
+      <input type="submit" value="Absenden">
+      <input type="reset">
+      <input type="hidden" name="sent" value="1">
+     </form>
+    <?php
+     echo "<form method=\"post\" action=\"index.php\">
+      <table>
+       <tr>
+        <td>
+         <input id=\"tags\" type=text name=suchstring size=24 class=\"ui-widget\">
+        </td>
+       </tr>
+       <tr>
+        <td>
+         <input type=\"submit\" value=\"Absenden\">
+         <input type=\"hidden\" name\"sent\" value=\"1\">
+        </td>
+       </tr>
+      </table>"
+    ?>
+  </aside>
+
+  <section id="content">
+   <article>
+    <h1>Inhalt</h1>
+
+     <?php
+      $navigation = isset($_GET['navi']) ? $_GET['navi'] : '0';
+      
+      switch ($navigation) {
+       case "0":
+        echo "<h2>Willkommen im Smartphone-Portal...</h2>";
+        break;
+       case "1":
+        echo "<h2>Smartphones anzeigen (normal)</h2>";
+        include("inc/smartphoneanzeigen1.inc.php");
+        break;
+       case "2":
+        echo "<h2>Smartphones anzeigen (Tablesorter)</h2>";
+        include("inc/smartphoneanzeigen2.inc.php");
+        break;
+       case "3":
+        echo "<h2>Smartphones anzeigen (Animiert: table-sort)</h2>";
+        include("inc/smartphoneanzeigen3.inc.php");
+        break;
+       case "4":
+        echo "<h2>Smartphones hinzuf&uuml;gen</h2>";
+        include("inc/smartphonehinzufuegen.inc.php");
+        break;
+      }
+
+      $sent = isset($_POST['sent']) ? $_POST['sent'] : '';
+      $suche = isset($_POST['suchstring']) ? $_POST['suchstring'] : '';
+
+      if ($sent and $suche) {
+       echo "<h2>Smartphone suchen</h2>";
+       include("suchen.inc.php");
+      }
+
+     ?>
+
+   </article>
+  </section>
+  
+  <footer>	
+   <h2>Wer braucht schon Smartphones?</h2>
+  </footer>
+
+ </div>
 </body>
 </html>
