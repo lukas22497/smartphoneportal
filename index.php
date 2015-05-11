10c3ee5ccadff0fba06f70a74992b55b7b4482d8
lukas22497 <lukas_mueller@outlook.de>
2015-05-11 08:24:28 +0200

    Bootstrap Quellcode in Index integriert

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
 
- <head>
-  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <link type="text/css" href="css/style.css" rel="stylesheet" media="screen" />
-
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <link type="text/css" href="css/style.css" rel="stylesheet" media="screen" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
   <title>Smartphone-Portal</title>
  </head>
 
@@ -90,6 +101,9 @@
    <h2>Wer braucht schon Smartphones?</h2>
   </footer>
 
- </div>
-</body>
+</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
